2cbb376452232072967dbdca4050d94b22bea304
Alexander Kurtakov <akurtako@redhat.com>
2016-03-24 00:09:48 +0200

    Move BREE of o.e.dltk.ruby.basicdebugger to Java 1.6
    With DLTK core being at 1.6 it makes no sense to stay lower in dependent
    modules.
    Also fix a new warning appeared after that. 
    
    Change-Id: I13199189f8a69f46722af547cd9c602262233b98
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- plugins/org.eclipse.dltk.ruby.basicdebugger/src/org/eclipse/dltk/ruby/basicdebugger/RubyBasicDebuggerRunnerFactory.java
+++ plugins/org.eclipse.dltk.ruby.basicdebugger/src/org/eclipse/dltk/ruby/basicdebugger/RubyBasicDebuggerRunnerFactory.java
@@ -1,11 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- 
  *******************************************************************************/
 
 package org.eclipse.dltk.ruby.basicdebugger;
@@ -14,8 +13,8 @@ import org.eclipse.dltk.launching.IInterpreterInstall;
 import org.eclipse.dltk.launching.IInterpreterRunner;
 import org.eclipse.dltk.launching.IInterpreterRunnerFactory;
 
-public class RubyBasicDebuggerRunnerFactory implements
-		IInterpreterRunnerFactory {
+public class RubyBasicDebuggerRunnerFactory implements IInterpreterRunnerFactory {
+	@Override
 	public IInterpreterRunner createRunner(IInterpreterInstall install) {
 		return new RubyBasicDebuggerRunner(install);
 	}
