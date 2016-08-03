a3ed72178a87016cf4143c010c748daefc9a9830
Alexander Kurtakov <akurtako@redhat.com>
2016-08-03 09:03:58 +0300

    Convert o.e.dltk.ruby.debug.tests to JUnit 4.
    * Convert RubyFilenameLinenumberTests.
    * Convert AllTests and add missing license header to it.
    * Dropped unneeded dep from MANIFEST.MF.
    
    Change-Id: If2d2456c49ca9e4caf4ccd0ed94f104efd675e65
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tests/org.eclipse.dltk.ruby.debug.tests/src/org/eclipse/dltk/ruby/debug/tests/AllTests.java
+++ tests/org.eclipse.dltk.ruby.debug.tests/src/org/eclipse/dltk/ruby/debug/tests/AllTests.java
@@ -1,20 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2016 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
 package org.eclipse.dltk.ruby.debug.tests;
-
-import junit.framework.Test;
-import junit.framework.TestSuite;
 
 import org.eclipse.dltk.ruby.debug.tests.console.RubyFilenameLinenumberTests;
 import org.eclipse.dltk.ruby.debug.tests.launching.RubyLaunchingTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RubyLaunchingTests.class, RubyFilenameLinenumberTests.class })
 public class AllTests {
-	public static Test suite() {
-		TestSuite suite = new TestSuite("org.eclipse.dltk.ruby.debug");
-		// $JUnit-BEGIN$
-
-		// Launching
-		suite.addTest(RubyLaunchingTests.suite());
-		suite.addTest(RubyFilenameLinenumberTests.suite());
-		// $JUnit-END$
-		return suite;
-	}
 }
--- tests/org.eclipse.dltk.ruby.debug.tests/src/org/eclipse/dltk/ruby/debug/tests/console/RubyFilenameLinenumberTests.java
+++ tests/org.eclipse.dltk.ruby.debug.tests/src/org/eclipse/dltk/ruby/debug/tests/console/RubyFilenameLinenumberTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 xored software, Inc.
+ * Copyright (c) 2008, 2016 xored software, Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,47 +11,48 @@
  *******************************************************************************/
 package org.eclipse.dltk.ruby.debug.tests.console;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import junit.framework.Test;
-
-import org.eclipse.dltk.core.tests.model.SuiteOfTestCases;
 import org.eclipse.dltk.ruby.debug.RubyFilenameLinenumberResolver;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-public class RubyFilenameLinenumberTests extends SuiteOfTestCases {
+public class RubyFilenameLinenumberTests {
 
-	public static Test suite() {
-		return new Suite(RubyFilenameLinenumberTests.class);
-	}
+	private static Pattern pattern;
 
-	private final Pattern pattern;
-
-	public RubyFilenameLinenumberTests(String name) {
-		super(name);
+	@BeforeClass
+	public static void setupClass() {
 		pattern = RubyFilenameLinenumberResolver.createPattern();
 	}
 
+	@Test
 	public void testSimple() throws Exception {
 		assertTrue(pattern.matcher("script.rb:2").matches());
 	}
 
+	@Test
 	public void testRoot() throws Exception {
 		assertTrue(pattern.matcher("/script.rb:2").matches());
 	}
 
+	@Test
 	public void testFullWindows() throws Exception {
 		assertTrue(pattern.matcher("C:/runtime/A/script.rb:2").matches());
 	}
 
+	@Test
 	public void testFullUnix() throws Exception {
-		assertTrue(pattern.matcher("/home/user/runtime/A/script.rb:2")
-				.matches());
+		assertTrue(pattern.matcher("/home/user/runtime/A/script.rb:2").matches());
 	}
 
+	@Test
 	public void testFrom() throws Exception {
-		Matcher m = pattern
-				.matcher("from c:/ruby/lib/ruby/site_ruby/1.8/rubygems.rb:61:in `require_gem'");
+		Matcher m = pattern.matcher("from c:/ruby/lib/ruby/site_ruby/1.8/rubygems.rb:61:in `require_gem'");
 		assertTrue(m.find());
 		assertEquals(2, m.groupCount());
 		assertEquals("61", m.group(2));
@@ -60,15 +61,17 @@ public class RubyFilenameLinenumberTests extends SuiteOfTestCases {
 		assertTrue(filename.endsWith("rubygems.rb"));
 	}
 
+	@Test
 	public void testShoulda() {
 		final String input = "c:/ruby/lib/ruby/gems/1.8/gems/Shoulda-1.2.0/lib/shoulda.rb:226:in `call'";
 		Matcher m = pattern.matcher(input);
 		assertTrue(m.find());
 	}
 
+	@Test
 	public void testNegativeMatch() {
 		// Matcher m = pattern
-		// .matcher("C:0:Warning: require_gem is obsolete.  Use gem instead.");
+		// .matcher("C:0:Warning: require_gem is obsolete. Use gem instead.");
 		// assertFalse(m.find());
 	}
 
